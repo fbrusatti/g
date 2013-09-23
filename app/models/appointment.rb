afdeba937aab539057446c2b72dd4673681ab502
Rodrigo Arino <rodrigoarino@gmail.com>
2013-09-23 20:51:23 -0300

    added path to button, and factorized code

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,9 +3,9 @@ class Appointment < ActiveRecord::Base
    :status,:priority, :description, :user_id, :customer_id,
 :property_id
   # == Associations
-  belongs_to  :user, foreign_key: "user_id"
-  belongs_to :property, foreign_key: "property_id"
-  belongs_to :customer, foreign_key: "customer_id"
+  belongs_to  :user
+  belongs_to :property
+  belongs_to :customer
 
   validates_presence_of :title, :start_date, :model, :user
 end
