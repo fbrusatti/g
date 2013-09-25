f72e4a591a9d1a611262515b5aa63e2b1163bf78
Franco Brusatti <fbrusatti@gmail.com>
2013-09-25 11:50:37 -0300

    Add default capybara test suite

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :email do |n|
+    "user#{n}@mail.com"
+  end
+
   factory :user do
+    email
+    password "pawssword"
+    password_confirmation "pawssword"
   end
 end
