aec8dd0ae1a6eef412f9010f7869a9e51ad23681
saul coria <coriasaul@gmail.com>
2013-10-06 21:23:17 -0300

    Add Validate presence the customer's phones

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,6 +9,7 @@ class Customer < ActiveRecord::Base
   # == Validations
   validates_presence_of :name, :surname
   validates_format_of :email, { :allow_blank => true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validate :presence_phone_or_mobile
 
   # == Associations
   belongs_to :user
@@ -23,5 +24,11 @@ class Customer < ActiveRecord::Base
       self.property_id = ids
   end
 
+  private
+    def presence_phone_or_mobile
+      if phones[:phone].blank? && phones[:mobile_phone].blank?
+        errors.add(:phones, I18n.t('activerecord.models.errors.message_phones'))
+      end
+    end
 
 end
