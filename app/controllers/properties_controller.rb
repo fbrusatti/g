d32b5117105347b724ddcd691c1b5c533b9df6f8
edu <edu.depetris.00@gmail.com>
2013-10-08 16:37:22 -0300

    Add transaction to views and controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,7 +2,6 @@ class PropertiesController < ApplicationController
   respond_to :html
   before_filter :authenticate_user!
 
-
   def index
     respond_to do |format|
       format.html { @properties = Property.all}
@@ -12,16 +11,19 @@ class PropertiesController < ApplicationController
 
   def new
     @property = Property.new
+    set_up_money
   end
 
   def create
     @property = current_user.properties.build(params[:property])
+    type_transaction(params)
     if @property.save
       flash[:success] = t('flash.property', message: t('flash.created'))
+    else
+      set_up_money
     end
     respond_with @property
   end
-
 
   def show
     @property = Property.find(params[:id])
@@ -29,13 +31,29 @@ class PropertiesController < ApplicationController
 
   def edit
     @property = Property.find(params[:id])
+    set_up_money
   end
 
   def update
     @property = Property.find(params[:id])
+    type_transaction(params)
+
     if @property.update_attributes(params[:property])
       flash[:success] = t('flash.property', message: t('flash.updated'))
+    else
+      set_up_money
     end
     respond_with @property
   end
+
+  private
+    def type_transaction(params)
+      text = "#{params[:to_sale]} #{params[:to_rent]}".strip
+      @property.type_transaction = text
+    end
+
+    def set_up_money
+      @property.build_money_to_sale if @property.money_to_sale.nil?
+      @property.build_money_to_rent if @property.money_to_rent.nil?
+    end
 end
