ba956585a1e1fa36545eaa913077f7dd231267d5
edu <edu.depetris.00@gmail.com>
2013-10-23 10:47:25 -0300

    Remove older fields

--- db/schema.rb
+++ db/schema.rb
@@ -41,8 +41,6 @@ ActiveRecord::Schema.define(:version => 20131017143056) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
-    t.string   "surname"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
